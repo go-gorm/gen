1970dee8bddaf9fc3fe8fdf2cedda941ec9b33da
iDer <idersec@gmail.com>
2021-08-26 17:46:11 +0800

    feat: struct member support ScanValuer interface

--- field/field.go
+++ field/field.go
@@ -6,7 +6,7 @@ import (
 	"gorm.io/gorm/clause"
 )
 
-type ScannerValuer interface {
+type ScanValuer interface {
 	Scan(src interface{}) error   // sql.Scanner
 	Value() (driver.Value, error) // driver.Valuer
 }
@@ -14,35 +14,35 @@ type ScannerValuer interface {
 // Field a standard field struct
 type Field struct{ expr }
 
-func (field Field) Eq(value ScannerValuer) Expr {
+func (field Field) Eq(value ScanValuer) Expr {
 	return expr{expression: clause.Eq{Column: field.RawExpr(), Value: value}}
 }
 
-func (field Field) In(values ...ScannerValuer) Expr {
+func (field Field) In(values ...ScanValuer) Expr {
 	return expr{expression: clause.IN{Column: field.RawExpr(), Values: field.toSlice(values...)}}
 }
 
-func (field Field) Gt(value ScannerValuer) Expr {
+func (field Field) Gt(value ScanValuer) Expr {
 	return expr{expression: clause.Gt{Column: field.RawExpr(), Value: value}}
 }
 
-func (field Field) Gte(value ScannerValuer) Expr {
+func (field Field) Gte(value ScanValuer) Expr {
 	return expr{expression: clause.Gte{Column: field.RawExpr(), Value: value}}
 }
 
-func (field Field) Lt(value ScannerValuer) Expr {
+func (field Field) Lt(value ScanValuer) Expr {
 	return expr{expression: clause.Lt{Column: field.RawExpr(), Value: value}}
 }
 
-func (field Field) Lte(value ScannerValuer) Expr {
+func (field Field) Lte(value ScanValuer) Expr {
 	return expr{expression: clause.Lte{Column: field.RawExpr(), Value: value}}
 }
 
-func (field Field) Like(value ScannerValuer) Expr {
+func (field Field) Like(value ScanValuer) Expr {
 	return expr{expression: clause.Like{Column: field.RawExpr(), Value: value}}
 }
 
-func (field Field) toSlice(values ...ScannerValuer) []interface{} {
+func (field Field) toSlice(values ...ScanValuer) []interface{} {
 	slice := make([]interface{}, len(values))
 	for i, v := range values {
 		slice[i] = v
