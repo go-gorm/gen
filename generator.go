f119a1411682d03ae2665a14f72704e6c7cf4e19
riverchu <churiver@outlook.com>
2021-09-20 20:23:21 +0800

    feat: operate field by column name

--- generator.go
+++ generator.go
@@ -140,7 +140,7 @@ var (
 	FieldIgnore = func(columnNames ...string) check.MemberOpt {
 		return func(m *check.Member) *check.Member {
 			for _, name := range columnNames {
-				if m.Name == name {
+				if m.ColumnName == name {
 					return nil
 				}
 			}
@@ -155,7 +155,7 @@ var (
 		}
 		return func(m *check.Member) *check.Member {
 			for _, reg := range regs {
-				if reg.MatchString(m.Name) {
+				if reg.MatchString(m.ColumnName) {
 					return nil
 				}
 			}
@@ -165,7 +165,7 @@ var (
 	// FieldRename specify field name in generated struct
 	FieldRename = func(columnName string, newName string) check.MemberOpt {
 		return func(m *check.Member) *check.Member {
-			if m.Name == columnName {
+			if m.ColumnName == columnName {
 				m.Name = newName
 			}
 			return m
@@ -174,7 +174,7 @@ var (
 	// FieldType specify field type in generated struct
 	FieldType = func(columnName string, newType string) check.MemberOpt {
 		return func(m *check.Member) *check.Member {
-			if m.Name == columnName {
+			if m.ColumnName == columnName {
 				m.Type = newType
 				m.ModelType = newType
 			}
@@ -185,7 +185,7 @@ var (
 	FieldTypeReg = func(columnNameReg string, newType string) check.MemberOpt {
 		reg := regexp.MustCompile(columnNameReg)
 		return func(m *check.Member) *check.Member {
-			if reg.MatchString(m.Name) {
+			if reg.MatchString(m.ColumnName) {
 				m.Type = newType
 				m.ModelType = newType
 			}
@@ -195,7 +195,7 @@ var (
 	// FieldTag specify json tag and gorm tag
 	FieldTag = func(columnName string, gormTag, jsonTag string) check.MemberOpt {
 		return func(m *check.Member) *check.Member {
-			if m.Name == columnName {
+			if m.ColumnName == columnName {
 				m.GORMTag, m.JSONTag = gormTag, jsonTag
 			}
 			return m
@@ -204,7 +204,7 @@ var (
 	// FieldJSONTag specify json tag
 	FieldJSONTag = func(columnName string, jsonTag string) check.MemberOpt {
 		return func(m *check.Member) *check.Member {
-			if m.Name == columnName {
+			if m.ColumnName == columnName {
 				m.JSONTag = jsonTag
 			}
 			return m
@@ -213,7 +213,7 @@ var (
 	// FieldGORMTag specify gorm tag
 	FieldGORMTag = func(columnName string, gormTag string) check.MemberOpt {
 		return func(m *check.Member) *check.Member {
-			if m.Name == columnName {
+			if m.ColumnName == columnName {
 				m.GORMTag = gormTag
 			}
 			return m
@@ -222,7 +222,7 @@ var (
 	// FieldNewTag add new tag
 	FieldNewTag = func(columnName string, newTag string) check.MemberOpt {
 		return func(m *check.Member) *check.Member {
-			if m.Name == columnName {
+			if m.ColumnName == columnName {
 				m.NewTag += " " + newTag
 			}
 			return m
