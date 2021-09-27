03725eccd6f95680ff8af519c6d08e75ff6523ae
riverchu <churiver@outlook.com>
2021-09-27 17:32:34 +0800

    feat: refactor Save

--- do.go
+++ do.go
@@ -378,7 +378,7 @@ func (d *DO) CreateInBatches(value interface{}, batchSize int) error {
 }
 
 func (d *DO) Save(value interface{}) error {
-	return d.db.Save(value).Error
+	return d.db.Clauses(clause.OnConflict{UpdateAll: true}).Create(value).Error
 }
 
 func (d *DO) First() (result interface{}, err error) {
