45bd16a990665d6aa14be4af69242b82d450f7f0
r1v3r <churiver@outlook.com>
2021-12-29 11:45:41 +0800

    test(field): add examples

--- field/export_test.go
+++ field/export_test.go
@@ -90,6 +90,10 @@ func TestExpr_Build(t *testing.T) {
 			Result: "GROUP_CONCAT(`id`)",
 		},
 		{
+			Expr:   field.Func.UnixTimestamp,
+			Result: "UNIX_TIMESTAMP()",
+		},
+		{
 			Expr:         field.Func.UnixTimestamp.Mul(99),
 			Result:       "(UNIX_TIMESTAMP())*?",
 			ExpectedVars: []interface{}{uint64(99)},
