b1cdee65ff6aa4b9c681beeab4e408815ed306b4
Francis Zhou <francis.tm@gmail.com>
2022-01-30 20:10:04 +0800

    add COLUMN_KEY query statement

--- internal/check/postgres/sql.go
+++ internal/check/postgres/sql.go
@@ -5,14 +5,23 @@ const (
 SELECT
 	c.table_name AS "TABLE_NAME",
 	c.column_name AS "COLUMN_NAME",
-	c.is_nullable AS "IS_NULLABLE",
+	CASE WHEN pd.description IS NULL THEN '' ELSE pd.description END AS "COLUMN_COMMENT",
+	c.data_type AS "DATA_TYPE",
+	CASE WHEN tc.constraint_type = 'PRIMARY KEY' THEN 'PRI' ELSE '' END AS "COLUMN_KEY",
 	c.udt_name AS "COLUMN_TYPE",
-	pd.description AS "COLUMN_COMMENT"
+	c.is_nullable AS "IS_NULLABLE"
 FROM
 	pg_catalog.pg_statio_all_tables psat
 INNER JOIN information_schema."columns" c ON
 	c.table_schema = psat.schemaname
 	AND c.table_name = psat.relname
+LEFT JOIN information_schema.constraint_column_usage ccu ON
+	ccu.table_schema = psat.schemaname
+	AND ccu.table_name = psat.relname
+	AND ccu.column_name = c.column_name
+LEFT JOIN information_schema.table_constraints tc ON
+	tc.constraint_schema = psat.schemaname
+	AND tc.constraint_name = ccu.constraint_name
 LEFT JOIN pg_catalog.pg_description pd ON
 	pd.objoid = psat.relid
 	AND pd.objsubid = c.ordinal_position
