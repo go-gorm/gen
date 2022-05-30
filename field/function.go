2bcab14a261e847baa4197fe32311726afddd187
r1v3r <churiver@outlook.com>
2022-05-30 15:22:08 +0800

    feat: support FromUnixTime

--- field/function.go
+++ field/function.go
@@ -1,6 +1,8 @@
 package field
 
 import (
+	"strings"
+
 	"gorm.io/gorm/clause"
 )
 
@@ -9,9 +11,18 @@ var Func = new(function)
 
 type function struct{}
 
+// UnixTimestamp same as UNIX_TIMESTAMP([date])
 func (f *function) UnixTimestamp(date ...string) Uint64 {
 	if len(date) > 0 {
 		return Uint64{expr{e: clause.Expr{SQL: "UNIX_TIMESTAMP(?)", Vars: []interface{}{date[0]}}}}
 	}
 	return Uint64{expr{e: clause.Expr{SQL: "UNIX_TIMESTAMP()"}}}
+}
+
+// FromUnixTime FROM_UNIXTIME(unix_timestamp[,format])
+func (f *function) FromUnixTime(date uint64, format string) String {
+	if strings.TrimSpace(format) != "" {
+		return String{expr{e: clause.Expr{SQL: "FROM_UNIXTIME(?, ?)", Vars: []interface{}{date, format}}}}
+	}
+	return String{expr{e: clause.Expr{SQL: "FROM_UNIXTIME(?)", Vars: []interface{}{date}}}}
 }
