378486834a4bfc2830892187760d6d0650d6af5c
r1v3r <churiver@outlook.com>
2022-08-27 18:59:35 +0800

    tests: test init

--- tests/go.mod
+++ tests/go.mod
@@ -3,7 +3,9 @@ module gorm.io/gen/tests
 go 1.16
 
 require (
+	github.com/mattn/go-sqlite3 v1.14.15 // indirect
 	gorm.io/driver/mysql v1.3.6
+	gorm.io/driver/sqlite v1.3.6
 	gorm.io/gorm v1.23.8
 )
 
--- tests/tests_test.go
+++ tests/tests_test.go
@@ -1,22 +1,91 @@
 package tests_test
 
 import (
-	"fmt"
+	"log"
 	"os"
+	"path/filepath"
 
 	"gorm.io/driver/mysql"
+	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+var DB *gorm.DB
+var (
+	mysqlDSN     = "gen:gen@tcp(localhost:9910)/gen?charset=utf8&parseTime=True&loc=Local"
+	postgresDSN  = "user=gen password=gen dbname=gen host=localhost port=9920 sslmode=disable TimeZone=Asia/Shanghai"
+	sqlserverDSN = "sqlserver://gen:LoremIpsum86@localhost:9930?database=gen"
 )
 
 func init() {
-	dsn := os.Getenv("GORM_DSN")
-	if dsn == "" {
-		dsn = "gen:gen@tcp(localhost:9910)/gen?charset=utf8&parseTime=True&loc=Local"
+	log.Print("initing...")
+	var err error
+	if DB, err = OpenTestConnection(); err != nil {
+		log.Printf("failed to connect database, got error %v", err)
+		os.Exit(1)
+	} else {
+		sqlDB, err := DB.DB()
+		if err != nil {
+			log.Printf("failed to connect database, got error %v", err)
+			os.Exit(1)
+		}
+
+		err = sqlDB.Ping()
+		if err != nil {
+			log.Printf("failed to ping sqlDB, got error %v", err)
+			os.Exit(1)
+		}
+
+		// RunMigrations()
+		if DB.Dialector.Name() == "sqlite" {
+			DB.Exec("PRAGMA foreign_keys = ON")
+		}
+	}
+	RunMigrations()
+}
+
+func OpenTestConnection() (db *gorm.DB, err error) {
+	dbDSN := os.Getenv("GEN_DSN")
+	switch os.Getenv("GORM_DIALECT") {
+	case "mysql":
+		log.Println("testing mysql...")
+		if dbDSN == "" {
+			dbDSN = mysqlDSN
+		}
+		db, err = gorm.Open(mysql.Open(dbDSN), &gorm.Config{})
+	default:
+		log.Println("testing sqlite3...")
+		db, err = gorm.Open(sqlite.Open(filepath.Join(os.TempDir(), "gorm.db")), &gorm.Config{})
 	}
 
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
-		panic(fmt.Errorf("open mysql fail: %w", err))
+		return
 	}
-	_ = db
+
+	if debug := os.Getenv("DEBUG"); debug == "true" {
+		db.Logger = db.Logger.LogMode(logger.Info)
+	} else if debug == "false" {
+		db.Logger = db.Logger.LogMode(logger.Silent)
+	}
+
+	return
+}
+
+var tableMetas = [][2]string{
+	{"DROP TABLE IF EXISTS `demo`;", "CREATE TABLE `demo` ( `id` int(11) NOT NULL AUTO_INCREMENT, PRIMARY KEY (`id`) ) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4;"},
+}
+
+func RunMigrations() {
+	db := DB.Session(&gorm.Session{})
+
+	for _, meta := range tableMetas {
+		dropTable, createTable := meta[0], meta[1]
+		if err := db.Exec(dropTable).Error; err != nil {
+			log.Printf("drop table fail: %s", err)
+		}
+		if err := db.Exec(createTable).Error; err != nil {
+			log.Printf("create table fail: %s", err)
+		}
+	}
 }
