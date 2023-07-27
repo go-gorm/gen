f380e777a70e6c676879a22463ad35b3e2a191cf
r1v3r <churiver@outlook.com>
2023-07-27 17:04:12 +0800

    tests: update unit test

--- tests/.expect/dal_3/query/credit_cards.gen_test.go
+++ tests/.expect/dal_3/query/credit_cards.gen_test.go
@@ -126,7 +126,7 @@ func Test_creditCardQuery(t *testing.T) {
 	}
 
 	var _a _another
-	var _aPK = field.NewString(_a.TableName(), clause.PrimaryKey)
+	var _aPK = field.NewString(_a.TableName(), "id")
 
 	err = _do.Join(&_a, primaryKey.EqCol(_aPK)).Scan(map[string]interface{}{})
 	if err != nil {
