9ded900a3949af77f96908df05de1b292aae1da7
E-Zou Shen <ezoushen@gmail.com>
2024-10-08 01:03:36 +0800

    feat: support RANDOM()

--- field/function.go
+++ field/function.go
@@ -30,3 +30,7 @@ func (f *function) FromUnixTime(date uint64, format string) String {
 func (f *function) Rand() String {
 	return String{expr{e: clause.Expr{SQL: "RAND()"}}}
 }
+
+func (f *function) Random() String {
+	return String{expr{e: clause.Expr{SQL: "RANDOM()"}}}
+}
