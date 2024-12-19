47b51ddfa7119d10fe858966af95fd27741c4487
qqxhb <1252905006@qq.com>
2024-12-19 20:55:24 +0800

    feat: interface template add method Rows

--- tests/.expect/dal_3/query/customers.gen.go
+++ tests/.expect/dal_3/query/customers.gen.go
@@ -6,6 +6,7 @@ package query
 
 import (
 	"context"
+	"database/sql"
 
 	"gorm.io/gorm"
 	"gorm.io/gorm/clause"
@@ -167,6 +168,8 @@ type ICustomerDo interface {
 	FirstOrCreate() (*model.Customer, error)
 	FindByPage(offset int, limit int) (result []*model.Customer, count int64, err error)
 	ScanByPage(result interface{}, offset int, limit int) (count int64, err error)
+	Rows() (*sql.Rows, error)
+	Row() *sql.Row
 	Scan(result interface{}) (err error)
 	Returning(value interface{}, columns ...string) ICustomerDo
 	UnderlyingDB() *gorm.DB
